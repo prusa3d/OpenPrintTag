5e025d4624096a04c422a8e81d21a4976a6ea45c
Daniel Čejchan <czdanol@gmail.com>
2025-11-10 16:14:36 +0100

    Rework material tags docs table
    Rework the table so that is follows the categorization.
    
    BFW-8031

--- docs_src/generate.py
+++ docs_src/generate.py
@@ -1,10 +1,70 @@
-from generate_common import gen_doc_file, env, Column, enum_columns, dir, out_dir
+from generate_common import gen_doc_file, env, Column, dir, out_dir
+import vars
 import shutil
+import io
+import yaml
+import os
 
-env.globals["material_tag_columns"] = enum_columns + [
-    Column(field="implies", title="Implies", transform=lambda x: ", ".join(map(lambda y: f"`{y}`", x)) if x else ""),
-    Column(field="hints", title="Hints", transform=lambda x: ", ".join(map(lambda y: f"`{y}`", x)) if x else ""),
-]
+
+def gen_material_tag_table():
+    r = io.StringIO("")
+    r.write("<table>")
+    r.write("<tr><th>ID</th><th>Name</th><th>Display name</th><th>Info</th>")
+
+    tags = yaml.safe_load(open(os.path.join(vars.data_dir, "tags_enum.yaml"), "r"))
+    categories = yaml.safe_load(open(os.path.join(vars.data_dir, "tag_categories_enum.yaml"), "r"))
+
+    categories_keys = {c["name"] for c in categories}
+
+    # Check that all tags have a matching category
+    for tag in tags:
+        if tag.get("deprecated", False):
+            continue
+
+        assert tag["category"] in categories_keys, f"Tag {tag['name']} category {tag['category']} is not in material_tag_categories.yaml"
+
+    for cat in categories:
+        r.write(f"<tr><th colspan='4' align='left'>{cat['emoji']} {cat['display_name']}</th></tr>")
+
+        for tag in tags:
+            if tag.get("deprecated", False):
+                continue
+
+            if tag["category"] != cat["name"]:
+                continue
+
+            r.write("<tr>")
+            r.write(f"<td>{tag['key']}</td>")
+            r.write(f"<td><code>{tag['name']}</code></td>")
+            r.write(f"<td>{tag['display_name']}</td>")
+
+            r.write("<td>")
+
+            desc_lines = []
+
+            desc = tag.get("description", [])
+            if isinstance(desc, list):
+                desc_lines += desc
+            elif len(desc.strip()) > 0:
+                desc_lines.append(desc)
+
+            implies = tag.get("implies", [])
+            if len(implies) > 0:
+                desc_lines.append("Implies " + ", ".join(map(lambda i: f"<code>{i}</code>", implies)))
+
+            hints = tag.get("hints", [])
+            if len(hints) > 0:
+                desc_lines.append("Hints " + ", ".join(map(lambda i: f"<code>{i}</code>", hints)))
+
+            r.write("<br>".join(desc_lines))
+            r.write("</td></tr>")
+
+    r.write("</table>")
+
+    return r.getvalue()
+
+
+env.globals["material_tag_table"] = gen_material_tag_table
 
 env.globals["material_type_columns"] = [
     Column(field="key", title="Key"),
